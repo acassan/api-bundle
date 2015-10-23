175fcc9942eec3be02142d12ce8d39dec3ad6042
Alexis <cassanfrp@gmail.com>
2015-10-23 16:33:49 +0200

    add api class

--- DependencyInjection/ApiExtension.php
+++ DependencyInjection/ApiExtension.php
@@ -5,8 +5,7 @@ namespace ApiBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Config\FileLocator;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -24,5 +23,8 @@ class ApiExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $container->setParameter($this->getAlias().'.config', $config);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
